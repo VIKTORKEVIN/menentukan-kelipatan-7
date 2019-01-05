c4c67495396933464a54e4dfcfae0c8332e7771b
VIKTORKEVIN <46395918+VIKTORKEVIN@users.noreply.github.com>
2019-01-05 18:53:45 +0700

    Add files via upload

---
+++ menampilkan kelipatan 7 habis dibagi 2.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int();
+    printf("KELIPATAN 7 YANG HABIS DIBAGI 2 DARI 1-50.\n\n");
+    int i=7;
+    while(i<50)
+    {
+        if(i%2==0 && i%7==0)
+        {
+            printf("\n%d",i);
+        }
+        i++;
+    }
+}
